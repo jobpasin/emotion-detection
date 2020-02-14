0de5f8d951f56aba8cf8bcb601233542133ea407
Pasin <jobpasin@hotmail.com>
2020-02-14 21:47:40 +0900

    Update function to merge 1dpca_train.py with 1dpca_train_eigenval.py

--- 1DPCA_Train_EigenVal.py
+++ 1DPCA_Train_EigenVal.py
@@ -1,4 +1,5 @@
 # Training Process
+# Reference: https://www.mitpressjournals.org/doi/10.1162/jocn.1991.3.1.71
 
 # !/usr/bin/python
 import os, sys
@@ -14,26 +15,28 @@ from PIL import Image
 
 # -------------------Initialize ----------------------------------
 percent_Eig = [80.0, 80.0, 80.0, 80.0]  # Minimum of percentage of eigenvalue
-num_Pic = 5  # Number of Pictures
+num_Eig = 16                                #Number of Eig
+num_Pic = 21                                #Number of Pictures
 image_dir = './TrainingPicsAll'  # Training pictures path
 img_index = []
 # img_index = ['1008.jpg', '1011.jpg', '1014.jpg', '1015.jpg', '1022.jpg', '1025.jpg',
 #              '1027.jpg', '1028.jpg', '1031.jpg', '1032.jpg', '1033.jpg', '1117.jpg',
 #              '1118.jpg', '1123.jpg', '1126.jpg', '1129.jpg']
-PCA_Path = './TrainedData/new_test_6.npz'  # Path to save PCA
+PCA_Path = './TrainedData/1DPCA_2_Test.npz'  # Path to save PCA
 
 # ----------------------------------------------------------------
 
 tic = timeit.default_timer()
 
 
-def import_images(path, num_pic, emotion, img_indexes=None):
+def import_images(path, emotion, img_indexes=None, img_amount=None):
     """
     Fetch list of images directory of specific emotion. Specifically work for our file format.
     :param path:        Folder directory where images are stored. Can be list to fetch from multiple folders.
-    :param num_pic:
     :param emotion:     String of emotion name. Image name should start with emotion e.g. sad_1010.jpg
     :param img_indexes: List of string of specific image id you want e.g. [1010.jpg]. Use None to fetch all
+    :param img_amount:  Amount of image you want to fetch. Use None to fetch all
+    :return:            ndarray of size (img_amount, h*w) or (num_image, features)
     """
     if type(path) is not list:
         path = [path]
@@ -49,87 +52,102 @@ def import_images(path, num_pic, emotion, img_indexes=None):
         image_paths = image_paths + im_p
     image_paths.sort()
 
-    # Fetch image size
-    image_pil = Image.open(image_paths[0]).convert('L')
-    [h, w] = np.shape(np.array(image_pil, 'float32'))
+    if img_amount is None:
+        img_amount = len(image_paths)
+    else:
+        assert img_amount <= len(image_paths), "Requested too many images. Amount of image found is {}.".format(
+            len(image_paths))
 
-    # num_pic = len(image_paths)
-    im_av = np.array(np.zeros([h * w, 1], 'float32'))  # Create average value
-    # Label for each images
-    for i,image_path in enumerate(image_paths):
-        # Read the image and convert to grayscale, convert to matrix
-        image_pil = Image.open(image_path).convert('L')
-        # print image_path
-        image = np.array(image_pil, 'float32')
-        image_vec_temp = np.reshape(image, h * w, 'C').transpose()
-        if image_path == image_paths[0]:
-            image_vec = image_vec_temp
-        else:
-            image_vec = np.concatenate((image_vec, image_vec_temp), axis=1)
-        im_av = im_av + image_vec_temp
-        if i == num_pic:
-            break
-    im_av = im_av / num_pic
-    image_vec = image_vec - np.tile(im_av, (1, num_pic))
+    # Load all image and subtract the value by the average value
+    image_vector = np.stack([load_image(image_path) for image_path in image_paths[0:img_amount]])
+    image_average = np.tile(np.mean(image_vector, 0), (img_amount, 1)) / img_amount
+
+    image_vector = image_vector - image_average
     # return the images list and average
-    return image_vec, im_av
+    return image_vector, image_average
 
 
-def import_image(fullpath):  # Importing single image
-    image = np.array(Image.open(fullpath).convert('L'), 'float32')
+def load_image(full_path):  # Importing single image
+    image = np.array(Image.open(full_path).convert('L'), 'float32')
     [h, w] = np.shape(image)
-    image_vec = np.reshape(image, h * w, 'C').transpose()
+    image_vec = np.reshape(image, h * w, 'C')
     return image_vec
 
 
-def create_eigface(images, im_av, percent_Eig):  # Process of creating optimal projection axis
+def create_eigface(images, im_av, num_eigen=None, percent_eigen=None):  # Process of creating optimal projection axis
+    """
+    Function to create eigenface (Using Principal Component Analysis with some extra technique to save computation cost)
+    :param images           : ndarray of size (num_image, features)
+    :param num_eigen        : int, number of eigenfaces. Cannot be specified with percent_eigen
+    :param percent_eigen    : float, number of percentage of eigenvalues for determine number of eigenface
 
-    [Size, num_pic] = images.shape  # Number of training images
-    # Create pseudo covariance matrix
-    Cov = images.transpose() * images
+    """
+    if num_eigen is not None and percent_eigen is not None:
+        raise ValueError("Only one of 'num_eigen' or 'percent_eigen' can be specified")
+    if num_eigen is None and percent_eigen is None:
+        raise ValueError("Either 'num_eigen' or 'percent_eigen' need to be specified")
+
+    # Transpose so the dimension will be [features, num_pic].
+    # This will be more simple for the following computation
+    images = images.T
+    features, num_pic = np.shape(images)
+    # Create pseudo covariance matrix. This case will have matrix size [num_pic, num_pic]
+    # which can be much smaller than standard approach where cov matrix will be [features, features]
+    cov = np.matmul(images.transpose(), images)
 
     # Compute Eigenvalue and Eigenvector
-    [EgVal, EgVec_temp] = np.linalg.eig(Cov)
-    EgVec_temp = np.matrix(EgVec_temp)
-    EgVec = images * EgVec_temp
-    index = EgVal.argsort()[::-1]
-    EgVal = EgVal[index]
-    EgVec = EgVec[:, index]
-    # Calculate number of eigenfaces
-    Sum_EgVal = np.sum(EgVal, dtype='float32')
-    EgVal_percent = EgVal / Sum_EgVal * 100
-    num_Eig = 1
-    for i in range(1, num_Pic):
-        if np.sum(EgVal_percent[0:i]) > percent_Eig:
-            break
-        num_Eig = num_Eig + 1
+    eig_value, eig_vector_temp = np.linalg.eig(cov)
+
+    # Find true eigenvector
+    eig_vector = np.matmul(images, eig_vector_temp)  # Convert vector back since we do transpose of normal cov matrix
+
+    # Sort eigenvalue and correspond eigenvector by descending
+    index = eig_value.argsort()[::-1]
+    eig_value = eig_value[index]
+    eig_vector = eig_vector[:, index]
+
+    if percent_eigen is not None:  # percent_eigen mode
+        assert percent_eigen > 0 and percent_eigen < 100, "percent_eigen need to be between 0 to 100"
+        # Calculate amount of eigenfaces by select the minimum amount that has total eigenvalues higher than threshold
+        sum_eigenvalue = np.sum(eig_value, dtype='float32')
+        for i in range(1, num_pic):
+            if np.sum(eig_value[0:i]) > percent_eigen*sum_eigenvalue/100:
+                num_eigen = i
+                break
+    else:  # num_eigen mode
+        if num_pic < num_eigen:
+            num_eigen = num_pic
+            print("Warning: Eigenfaces is limited to no more than the number of training images")
+
+    # Fetch a selected number of eigenface and eigenvalue
+    eigenface = eig_vector[:,0:num_eigen]
+
     # Select only num_Eig vectors and normalize to size of 1
-    Eig = EgVec[:, 0] / np.linalg.norm(EgVec[:, 0])
-    for cnt in range(1, num_Eig):
-        Eig = np.concatenate((Eig, EgVec[:, cnt] / np.linalg.norm(EgVec[:, cnt])), axis=1)
+    # eigenface = eig_vector/eig_vector.sum(axis=0,keepdims=1)  # Unused
+
     # return optimal projection axis and average image for future use
-    return Eig, EgVal
+    return eigenface, eig_value
 
 
-def reconstruct_image(Eig, num_Eig, Mode):  # Return eigenface into image
+def reconstruct_image(eigenface, num_eigenface, mode):  # Return eigenface into image
     # ReconIm(Eig[0],0,'E') or RecomIm(Im_Av[0],0,'Av')
 
-    if Mode == "E":
-        Eig_Im = np.reshape(Eig[:, num_Eig], (100, 100))
+    if mode == "E":
+        Eig_Im = np.reshape(eigenface[:, num_eigenface], (100, 100))
         Eig_Im = Eig_Im - np.matrix.min(Eig_Im)
         Eig_Im = np.array(Eig_Im / np.max(Eig_Im) * 255, dtype=np.uint8)
-    elif Mode == "E2":
-        Eig_Im = np.reshape(Eig[:, num_Eig], (100, 100))
+    elif mode == "E2":
+        Eig_Im = np.reshape(eigenface[:, num_eigenface], (100, 100))
         Eig_Im = -Eig_Im
         Eig_Im = Eig_Im - np.matrix.min(Eig_Im)
         Eig_Im = np.array(Eig_Im / np.max(Eig_Im) * 255, dtype=np.uint8)
-    elif Mode == "Av":
-        Eig_Im = np.reshape(Eig, (100, 100))
+    elif mode == "Av":
+        Eig_Im = np.reshape(eigenface, (100, 100))
         Eig_Im = np.array(Eig_Im, dtype=np.uint8)
     else:
         print("Error mode : 'E' or 'Av'")
     Eig_Im = cv2.resize(Eig_Im, (400, 400))
-    cv2.imshow("Eigenface Image: " + str(num_Eig), Eig_Im)
+    cv2.imshow("Eigenface Image: " + str(num_eigenface), Eig_Im)
 
     cv2.waitKey(0)
     # cv2.destroyAllWindows()
